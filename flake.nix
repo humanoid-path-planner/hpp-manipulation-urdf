7fbc28ee37cf58b877bfe6558456a1fc15766db0
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-12-22 17:39:26 +0100

    nix flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Implementation of a parser for hpp-manipulation";
 
   inputs = {
-    nixpkgs.url = "github:gepetto/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/362956/head";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
