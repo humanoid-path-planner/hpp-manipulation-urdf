d11819d12234971dc3a7801cab2d3bfaa5d5dc82
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-12-17 02:35:25 +0100

    flake: sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -12,33 +12,37 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          packages = {
-            default = self'.packages.hpp-manipulation-urdf;
-            hpp-manipulation-urdf = pkgs.hpp-manipulation-urdf.overrideAttrs {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./doc
-                  ./include
-                  ./package.xml
-                  ./src
-                  ./tests
-                ];
-              };
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, self, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          { gepetto-pkgs.overlays = [ self.overlays.default ]; }
+        ];
+        flake.overlays.default = _final: prev: {
+          hpp-manipulation-urdf = prev.hpp-manipulation-urdf.overrideAttrs {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./CMakeLists.txt
+                ./doc
+                ./include
+                ./package.xml
+                ./src
+                ./tests
+              ];
             };
           };
         };
-    };
+        perSystem =
+          { pkgs, self', ... }:
+          {
+            packages = {
+              default = self'.packages.hpp-manipulation-urdf;
+              hpp-manipulation-urdf = pkgs.hpp-manipulation-urdf;
+            };
+          };
+      }
+    );
 }
